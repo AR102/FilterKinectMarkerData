526855132b6e39aa6afa629810ee4a88e350e967
Documenter.jl <documenter@juliadocs.github.io>
2023-01-26 12:04:44 +0000

    build based on 150014b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = FilterKinect","category":"page"},{"location":"#FilterKinect","page":"Home","title":"FilterKinect","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FilterKinect.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [FilterKinect]","category":"page"}]
+}
