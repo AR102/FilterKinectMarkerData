6123cc06a96dff509bc6882fe4efdaf06f48c0b9
AR102 <weltraumkugel@gmail.com>
2023-04-05 22:31:18 +0200

    added check for making sure user is in top-level project dir

--- scripts/preview_docs.sh
+++ scripts/preview_docs.sh
@@ -1,2 +1,12 @@
+# determine name of current directory (not full path)
+# https://stackoverflow.com/a/1371283
+dirname=${PWD##*/}
+dirname=${dirname:-/}
+
+# if current directory is "scripts", then go back up to top-level dir of package
+if [ "$dirname" = "scripts" ]; then
+    cd ..
+fi
+
 # using https://github.com/tlienart/LiveServer.jl/#serve-docs
 julia --project=docs -e 'using FilterKinect, LiveServer; servedocs()'
